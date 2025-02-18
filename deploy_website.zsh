a211bf8418412bfe12f1a2a2e6a186d54e93917f
pramasahayam <pranav.rr@icloud.com>
2025-02-18 11:07:24 -0600

    added "Enclosed" serivce

--- deploy_website.zsh
+++ deploy_website.zsh
@@ -3,5 +3,8 @@
 # Pull the latest docker-compose.yml
 git pull origin main  # Adjust the branch name if needed
 
+# Pull latest docker images
+docker compose pull
+
 # Run docker-compose up -d
 docker compose up -d
